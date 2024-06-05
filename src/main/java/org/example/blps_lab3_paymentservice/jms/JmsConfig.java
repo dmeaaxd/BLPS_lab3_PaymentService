9eebe4ead10e1c52e119c092fba5318cd7b6fda0
Maksim Ushakov <dmeaaxd@gmail.com>
2024-06-05 20:29:14 +0300

    fix jms conf

--- src/main/java/org/example/blps_lab3_paymentservice/jms/JmsConfig.java
+++ src/main/java/org/example/blps_lab3_paymentservice/jms/JmsConfig.java
@@ -31,7 +31,6 @@ public class JmsConfig {
         connectionFactory.setBrokerURL(brokerUrl);
         connectionFactory.setPassword(user);
         connectionFactory.setUserName(password);
-        connectionFactory.setTrustedPackages(Arrays.asList("org.example.jmstestservice.objects"));
         return connectionFactory;
     }
 
